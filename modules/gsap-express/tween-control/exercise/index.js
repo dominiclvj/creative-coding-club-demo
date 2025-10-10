d03c87ad83208abcdd26b1bbf1e939766cf3516b
Benedict Jackson <bavjackson@gmail.com>
2025-10-10 11:50:16 +0100

    format script

--- modules/gsap-express/tween-control/exercise/index.js
+++ modules/gsap-express/tween-control/exercise/index.js
@@ -4,9 +4,9 @@ document.addEventListener('click', function (event) {
   }
 })
 
-var tween = gsap.to(".green", {duration:3, x:600, ease:"linear", paused:true});
+var tween = gsap.to(".green", { duration: 3, x: 600, ease: "linear", paused: true });
 
-document.getElementById("play").onclick = ()=> tween.play();
-document.getElementById("pause").onclick = ()=> tween.pause();
-document.getElementById("reverse").onclick = ()=> tween.reverse();
+document.getElementById("play").onclick = () => tween.play();
+document.getElementById("pause").onclick = () => tween.pause();
+document.getElementById("reverse").onclick = () => tween.reverse();
 // use the code snippets above as a pattern to implement the tween.restart() method for our animation
